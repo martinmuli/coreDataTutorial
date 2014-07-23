059bcb747aff969ea582d3cd7bc14a12175061c0
MMuli <user@MuliMac.local>
2014-07-23 09:50:52 -0500

    Initial Commit

---
+++ CoreData Tutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreData Tutorial
+//
+//  Created by MMuli on 7/23/14.
+//  Copyright (c) 2014 com.btgold.ent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
